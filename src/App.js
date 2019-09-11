c9d203388991c263b66bf19bfa097e4163913288
MrGio7 <lashagiorgi1233@gmail.com>
2019-09-11 13:03:34 +0400

    added home page skelet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import './App.css';
+import './assets/SCSS/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>APP IS ONLINE</h1>
+      <div className="header">
+        <h1>header</h1>
+      </div>
+
+      <div className="body">
+        <h1>body</h1>
+      </div>
+
+      <div className="footer">
+        <div className='home'>
+          <h2>Home</h2>
+        </div>
+
+        <div className="categories">
+          <h2>Categ.</h2>
+        </div>
+
+        <div className="delivery">
+          <h2>deliv</h2>
+        </div>
+
+        
+        <div className="search">
+          <h2>Search</h2>
+        </div>
+
+        <div className="me">
+          <h2>Me</h2>
+        </div>
+      </div>
     </div>
   );
 }
