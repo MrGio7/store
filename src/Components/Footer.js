e91a7a53c04b12de7c6899f1b5ef75d3533e42fd
MrGio7 <lashagiorgi1233@gmail.com>
2019-09-12 12:41:53 +0400

    added footer styling

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,33 +9,54 @@ import HomePng from "../assets/IMG/Icons/Home.png";
 import MePng from "../assets/IMG/Icons/Me.png";
 import SearchPng from "../assets/IMG/Icons/Search.png";
 
-const Footer = () => {
+const Footer = props => {
+  const path = window.location.pathname;
   return (
     <div className="footer">
       <div className="home">
         <Link to="/">
-          <img src={HomePng} alt="Home" />
+          <img
+            src={HomePng}
+            alt="Home"
+            className={path === "/" ? "selected" : null}
+          />
           <h2>Home</h2>
         </Link>
       </div>
 
       <div className="categories">
-        <img src={CategoryPng} alt="categories" />
+        <img
+          src={CategoryPng}
+          alt="categories"
+          className={path === "/categories" ? "selected" : null}
+        />
         <h2>Categ.</h2>
       </div>
 
       <div className="delivery">
-        <img src={DeliveryPng} alt="Delivery" />
+        <img
+          src={DeliveryPng}
+          alt="Delivery"
+          className={path === "/delivery" ? "selected" : null}
+        />
         <h2>deliv</h2>
       </div>
 
       <div className="search">
-        <img src={SearchPng} alt="Search" />
+        <img
+          src={SearchPng}
+          alt="Search"
+          className={path === "/search" ? "selected" : null}
+        />
         <h2>Search</h2>
       </div>
 
       <div className="me">
-        <img src={MePng} alt="account" />
+        <img
+          src={MePng}
+          alt="account"
+          className={path === "/account" ? "selected" : null}
+        />
         <h2>Me</h2>
       </div>
     </div>
