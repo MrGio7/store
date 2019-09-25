ff5980ac7461f8c55373cc47f7310c69a6d2bf07
MrGio7 <lashagiorgi1233@gmail.com>
2019-09-25 20:25:23 +0400

    improved categories section design

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import '../assets/SCSS/Categories.scss';
+
 const Categories = props => {
     
     return(
         props.loading ? <h1>loading</h1> : (
-            <div className="categories">
+            <div className="categoriesSection">
                 {props.categories.map(each => {
                     return(
-                        <img src={each.img} alt="category" />
+                        <div>
+                        <img src={each.img} key={each.id} alt="category" />
+                        <h1>{each.category}</h1>
+                        </div>
                     )
                 })}
             </div>
