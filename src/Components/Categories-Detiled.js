edab4f3dcf33c789aaf0b5bfcbc863702374da38
MrGio7 <lashagiorgi1233@gmail.com>
2019-10-13 21:35:15 +0400

    added detied categories

--- src/Components/Categories-Detiled.js
+++ src/Components/Categories-Detiled.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 const CategoriesDetiled = props => {
-    console.log(props);
+    const id = props.match.params.id;
+
+    const restaurants = props.categories.filter(each => each.id.toString() === id)[0].restaurants;
+
     return (
         <div className="detiledCategories">
-            
+            {restaurants.map((each, index) => {
+                return (
+                    <div className="restaurant" key={index}>
+                        <img src={each.img} alt={each.restaurants} />
+                        <h3>{each.restaurants}</h3>
+                    </div>
+                )
+            })}
         </div>
     )
 }
