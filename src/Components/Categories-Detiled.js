3923f117629ff802a18a526df6436bf971adef88
MrGio7 <lashagiorgi1233@gmail.com>
2019-10-19 11:59:52 +0400

    added detiled categories restaurants ling=ks

--- src/Components/Categories-Detiled.js
+++ src/Components/Categories-Detiled.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const CategoriesDetiled = props => {
     const id = props.match.params.id;
 
     const restaurants = props.categories.filter(each => each.id.toString() === id)[0].restaurants;
+    console.log(restaurants)
 
     return (
         <div className="detiledCategories">
             {restaurants.map((each, index) => {
                 return (
                     <div className="restaurants" key={index}>
-                        <img src={each.img} alt={each.restaurants} />
-                        <h3>{each.restaurants}</h3>
+                        <Link to={`/restaurants/${each.id}`}>
+                            <img src={each.img} alt={each.restaurants} />
+                            <h3>{each.restaurants}</h3>
+                        </Link>
                     </div>
                 )
             })}
