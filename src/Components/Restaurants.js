170130927b19c1c16e74fd1fd6102c7364bd1907
MrGio7 <lashagiorgi1233@gmail.com>
2019-10-22 01:29:51 +0400

    added expanded product styling

--- src/Components/Restaurants.js
+++ src/Components/Restaurants.js
@@ -8,6 +8,14 @@ const Restaurants = props => {
     const restaurant = props.restaurants.filter(each => each.id.toString() === id)[0];
     const productCategories = props.products.filter(each => each.RestaurantsID === restaurant.id);
 
+    const expand = ev => {
+        if(ev.currentTarget.className === "product") {
+            ev.currentTarget.className = "product expanded"
+        } else if (ev.currentTarget.className === "product expanded"){
+            ev.currentTarget.className = "product"
+        }
+    }
+
     return (
         <div className="restaurant">
             <div className="restaurantHeader">
@@ -22,21 +30,23 @@ const Restaurants = props => {
                         <div className="productCategory" key={eachProdCat.id}>
                             <h2>{eachProdCat.name}</h2>
 
-                            
                             {eachProdCat.products.map(eachProd => {
                                 return(
-                                     <div className="product" key={eachProd.id}>
+                                     <div className="product" key={eachProd.id} onClick={expand}>
                                          <div className="description">
                                              <h3>{eachProd.name}</h3>
                                              <h4>{eachProd.description}</h4>
                                              <h5>{eachProd.price}</h5>
+                                             <span>
+                                                 <h6>Count:</h6>
+                                                <input type="button" class="button" value="Add to order" /> 
+                                             </span>
                                          </div>
 
                                          <img src={eachProd.img} alt={eachProd.name} />
                                      </div>
                                 )
                             })}
-                            
                         </div>
                     )
                 })}
