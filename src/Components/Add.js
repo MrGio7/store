8a836b8c5ba9bbefad8f08e2c0492fa4cb20db22
MrGio7 <lashagiorgi1233@gmail.com>
2019-10-26 16:24:53 +0400

    finished select category btn

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -3,11 +3,11 @@ import React, { useState } from 'react';
 import '../assets/SCSS/Add.scss';
 
 const Add = props => {
-    const [btnName, setBtnName] = useState("Select Category");
+    const [btnName, setBtnName] = useState({name: "Select Category"});
 
     const selectCategory = ev => {
         ev.preventDefault();
-        setBtnName(ev.target.id);
+        setBtnName({id: ev.target.id, name: ev.target.innerText});
         ev.currentTarget.parentElement.style.display = "none";
     };
 
@@ -21,6 +21,7 @@ const Add = props => {
         }
     }
 
+ 
     return(
         <div className="add">
             <h1>Add New Place</h1>
@@ -30,16 +31,15 @@ const Add = props => {
 
                 <input type="text" placeholder="image address" />
 
-                <input type="button" value={btnName} className="dropbtn" onClick={catBtn} />
+                <input type="button" value={btnName.name} className="dropbtn" onClick={catBtn} />
                 <div className="myDropdown" style={{display: "none"}}>
-                    <input type="text" placeholder="Search..." id="searchCat" />
                     {props.categories.map(each => {
                         return(
-                            <h2 key={each.id} id={each.category} onClick={selectCategory}>{each.category}</h2>
+                            <h2 key={each.id} id={each.id} onClick={selectCategory}>{each.category}</h2>
                         )
                     })}
                 </div>
-                
+
                 <input type="button" className="btn" value="Submit" />
             </form>
         </div>
