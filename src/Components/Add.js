b0474df03cbd53583fb614ec6198be95f0c7ca32
MrGio7 <lashagiorgi1233@gmail.com>
2019-10-26 16:11:25 +0400

    added categories select and hide handlers

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../assets/SCSS/Add.scss';
 
-const Add = () => {
+const Add = props => {
+    const [btnName, setBtnName] = useState("Select Category");
+
+    const selectCategory = ev => {
+        ev.preventDefault();
+        setBtnName(ev.target.id);
+        ev.currentTarget.parentElement.style.display = "none";
+    };
+
+    const catBtn = () => {
+        const dropDown = document.getElementsByClassName("myDropdown")[0];
+        
+        if(dropDown.style.display === 'none'){
+            dropDown.style.display = 'block';
+        } else if(dropDown.style.display === 'block'){
+            dropDown.style.display = 'none';
+        }
+    }
 
     return(
         <div className="add">
@@ -10,8 +27,19 @@ const Add = () => {
 
             <form>
                 <input type="text" placeholder="name" />
+
                 <input type="text" placeholder="image address" />
-                <input type="select" placeholder="category" />
+
+                <input type="button" value={btnName} className="dropbtn" onClick={catBtn} />
+                <div className="myDropdown" style={{display: "none"}}>
+                    <input type="text" placeholder="Search..." id="searchCat" />
+                    {props.categories.map(each => {
+                        return(
+                            <h2 key={each.id} id={each.category} onClick={selectCategory}>{each.category}</h2>
+                        )
+                    })}
+                </div>
+                
                 <input type="button" className="btn" value="Submit" />
             </form>
         </div>
